0518ebce77722df411a42ca246ac7d8355433a92
Neuradigi01 <neuradigi01@gmail.com>
2025-05-19 20:52:20 +0530

    work is pending..

---
+++ src/app/user/buy-token/buy-token.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-buy-token',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './buy-token.component.html',
+  styleUrls: ['./buy-token.component.scss']
+})
+export class BuyTokenComponent {
+
+}
