da40ce0c16285e85827ce592b1ddf09f393e0a21
Ryder <pandeyabhishek234@gmail.com>
2025-05-21 18:49:04 +0530

    new contract on pol

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -20,7 +20,7 @@ export class AppSettings {
     public static readonly InternalTokenName = "Neuradigi";
 
     public static readonly IsTestNetworkEnabled = false;
-    public static readonly ContractAddress = this.IsTestNetworkEnabled ? '0x1D861F3F4e48C24981f171242419Cc59512fC9E4' : '0x2F2B5AB7E553a421BfF9A29f024B8586657c22A8';  //0xF9aF94304fc7A1aadB3c40f0e7840455a756D60c
+    public static readonly ContractAddress = this.IsTestNetworkEnabled ? '0x1D861F3F4e48C24981f171242419Cc59512fC9E4' : '0xc8324869F1dd3E3597e216bdB9f0D273951bfaBc';  //0xF9aF94304fc7A1aadB3c40f0e7840455a756D60c
     public static readonly ABI: any = abi;
 
     public static readonly IsPaymentCurrencyDifferentThanNative = true;
@@ -28,7 +28,7 @@ export class AppSettings {
     static CHAIN = this.IsTestNetworkEnabled ? Chain.BEP20.TestNet : Chain.BEP20.MainNet;
 
     public static readonly PaymentTokenSymbol = this.IsPaymentCurrencyDifferentThanNative ? "USDT" : this.CHAIN.NativeCurrency.Symbol;
-    public static readonly PaymentTokenContractAddress = this.IsTestNetworkEnabled ? '0x4622B46df102fD8648385bc96fd7a837111AeC61' : '0x541Db716243C6168911e1F406f520Ce67C0d4725';
+    public static readonly PaymentTokenContractAddress = this.IsTestNetworkEnabled ? '0x4622B46df102fD8648385bc96fd7a837111AeC61' : '0xbe923b00582b773aB812b44d8F6bE5002959399a';//'0x541Db716243C6168911e1F406f520Ce67C0d4725';
     public static readonly PaymentTokenABI: any = paymentTokenAbi;
     public static readonly PaymentTokenDecimals = 18;
 
